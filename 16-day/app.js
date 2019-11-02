1043acf3264112fa4948eec84b736188b201f5e0
CadillacJenkins <walter.k.jenkins@gmail.com>
2019-11-02 17:17:04 -0500

    built news feeds for diifferent categories

--- 16-day/app.js
+++ 16-day/app.js
@@ -1,41 +1,44 @@
 import {key} from './config.js'
 
-        const app = document.getElementById('app');
-        const nytkey = key();
-        
-        const getStories = ()=>{
-            fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=' + nytkey)
-            .then(response =>{
-                if(response.ok){
-                    return response.json();
-                }else{
-                    return Promise.reject(response);
-                }                  
-            }).then(data => {
-                console.log(data.results);
-                const html = data.results.map(d=>{
-                    const link = "<a href='" + d.url+ "'><h2>" + d.title + "</h2></a>"
-                    if(d.multimedia.length > 1){
-                        const pic = d.multimedia[2].url;
-                        return(
-                            "<div class='container' id='" + d.url + "'><div class='about'><div class='pic'><img src='" 
-                            + pic +
-                            "' ></div><div class='headline'>" + link + "<h3>" 
-                            + d.byline + "</h3><p>" + d.abstract + "</p></div></div></div>"
-                        );
-                    }
-                    return(
-                        "<div class='container' id='" + d.url + "'><h2>"
-                        + link + "</h2><h3>" + d.byline + "</h3><p>" + d.abstract + "</p></div></div>"
+const home = document.getElementById('home');
+const tech = document.getElementById('tech');
+const world = document.getElementById('world');
+const politics = document.getElementById('politics');
 
-                        );
-                    }).join('');
-                    app.innerHTML = html;
-                    return html;
-            }).catch(err => {
-                console.log("Error logged: ", err);
-            });
+const nytkey = key();
+const render=(articles, section)=>{
+    section[1].innerHTML = "<h1>" + section[2] + "</h1>" + articles.map(d=>{
+        const link = "<a href='" + d.url+ "'><h2>" + d.title + "</h2></a>";
+        if(d.multimedia.length > 1){
+            const pic = d.multimedia[2].url;
+            return(
+                "<div class='container' id='" + d.url + "'><div class='about'><div class='pic'><img src='" 
+                + pic +
+                "' ></div><div class='headline'>" + link + "<h3>" 
+                + d.byline + "</h3><p>" + d.abstract + "</p></div></div></div>"
+            );
         }
+        return(
+            "<div class='container' id='" + d.url + "'><h2>"
+            + link + "</h2><h3>" + d.byline + "</h3><p>" + d.abstract + "</p></div></div>"
 
-        getStories();
+            );
+        }).join('');
+}
+const list = [['home', home, "Top News"],['technology', tech, "Technology News"],['world', world, "World News"],['politics', politics, "Political News"]];
+    list.forEach(s=>{
+                fetch('https://api.nytimes.com/svc/topstories/v2/' + s[0] + '.json?api-key=' + nytkey)
+                .then(response =>{
+                    if(response.ok){
+                        return response.json();
+                    }else{
+                        return Promise.reject(response);
+                    }                  
+                }).then(data => {
+                    render(data.results, s);
+                }).catch(err => {
+                    console.log("Error logged: ", err);
+                });
+            });
+
 	
