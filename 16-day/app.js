126376bf22255d628b4e0bb6ed134c65d6db0926
CadillacJenkins <walter.k.jenkins@gmail.com>
2019-11-03 13:27:01 -0600

    updated to sanitize

--- 16-day/app.js
+++ 16-day/app.js
@@ -5,22 +5,28 @@ const tech = document.getElementById('tech');
 const world = document.getElementById('world');
 const politics = document.getElementById('politics');
 
+var sanitizeHTML = function (str) {
+    var temp = document.createElement('div')
+    temp.textContent = str
+    return temp.innerHTML
+  }
+
 const nytkey = key();
 const render=(articles, section)=>{
     section[1].innerHTML = "<h1>" + section[2] + "</h1>" + articles.map(d=>{
-        const link = "<a href='" + d.url+ "'><h2>" + d.title + "</h2></a>";
+        const link = "<a href='" + sanitizeHTML(d.url)+ "'><h2>" + sanitizeHTML( d.title) + "</h2></a>";
         if(d.multimedia.length > 1){
-            const pic = d.multimedia[2].url;
+            const pic = sanitizeHTML(d.multimedia[2].url);
             return(
-                "<div class='container' id='" + d.url + "'><div class='about'><div class='pic'><img src='" 
+                "<div class='container' id='" + sanitizeHTML(d.url) + "'><div class='about'><div class='pic'><img src='" 
                 + pic +
                 "' ></div><div class='headline'>" + link + "<h3>" 
-                + d.byline + "</h3><p>" + d.abstract + "</p></div></div></div>"
+                +  sanitizeHTML(d.byline) + "</h3><p>" + sanitizeHTML(d.abstract) + "</p></div></div></div>"
             );
         }
         return(
-            "<div class='container' id='" + d.url + "'><h2>"
-            + link + "</h2><h3>" + d.byline + "</h3><p>" + d.abstract + "</p></div></div>"
+            "<div class='container' id='" +  sanitizeHTML(d.url) + "'><h2>"
+            + link + "</h2><h3>" +  sanitizeHTML(d.byline) + "</h3><p>" +  sanitizeHTML(d.abstract) + "</p></div></div>"
 
             );
         }).join('');
@@ -41,4 +47,21 @@ const list = [['home', home, "Top News"],['technology', tech, "Technology News"]
                 });
             });
 
-	
+const sectDisp = (tab)=>{
+    home.style.display='none';
+    tech.style.display='none';
+    world.style.display='none';
+    politics.style.display='none';
+
+    tab.style.display = 'block';
+}
+
+
+
+homeBtn.addEventListener('click', event => sectDisp(home), false)
+techBtn.addEventListener('click', event => sectDisp(tech), false)
+worldBtn.addEventListener('click', event => sectDisp(world), false)
+politicsBtn.addEventListener('click', event => sectDisp(politics), false)
+
+
+sectDisp(home);
