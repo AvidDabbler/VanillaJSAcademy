02ad762dbdd357f8e06faddcb760296dc7e6805e
CadillacJenkins <walter.k.jenkins@gmail.com>
2019-11-10 18:02:49 -0600

    completed monsters app day 27

--- 23-day/app.js
+++ 23-day/app.js
@@ -1,4 +1,13 @@
 const app = document.getElementById('app');
+const score = document.getElementById('score');
+const reset = document.getElementById('reset');
+
+let standing = {
+    win: 0,
+    lose: 0,
+    correct: 0
+};
+
 
 var monsters = [
     'monster1',
@@ -42,16 +51,14 @@ const init= ()=>{
     shuffleMonsters = shuffle(monsters.slice());
 
     app.innerHTML = "<div class='row'>" + shuffleMonsters.map((monster, index) =>{
-        const html = "<div class='grid door'>"
-            + "<button data-monster-id ='" + index + "'>"
+        const html = "<div aria-live='polite' class='grid door'>"
+            + "<button class='monster-button' data-monster-id ='" + index + "'>"
                 + "<img id='" + index + "' alt='Click to find all the monsters' src='door.svg'></button></div>";
         return html;
     }).join('') + '</div>';
+    standing.correct = 0;
+
 }
-
-
-
-init();
 
 const clickDoor = el =>{
     const monster = el.target.closest('[data-monster-id]');
@@ -61,13 +68,23 @@ const clickDoor = el =>{
     }
     const id = monster.getAttribute('data-monster-id');
     if (shuffleMonsters[id] == 'sock'){
+        monster.parentNode.innerHTML = "<img alt='" + shuffleMonsters[id] + "' src='" + shuffleMonsters[id] +".svg'>";
         alert('You lost the game');
         init();
+        standing.lose++;
+        score.innerHTML = "<h3 class ='full' id = 'score'> Win: " + standing.win + " | Lose: " + standing.lose + "</h3>"; 
+    }else if(standing.correct < 10){
+        monster.parentNode.innerHTML = "<img alt='" + shuffleMonsters[id] + "' src='" + shuffleMonsters[id] +".svg'>";
+        standing.correct++;
+        console.log(standing);
     }else{
-        monster.parentNode.innerHTML = "<img alt='" + shuffleMonsters[id] + "' src='" + shuffleMonsters[id] +".svg'>"
+        alert('you won the game');
+        standing.win++;
+        score.innerHTML = "<h3 class ='full' id = 'score'> Win: " + standing.win + " | Lose: " + standing.lose + "</h3>"; 
+        init();
     }
 };
 
-
-app.addEventListener('click', clickDoor, false)
-
+reset.addEventListener('click', init, false);
+app.addEventListener('click', clickDoor, false);
+init();
