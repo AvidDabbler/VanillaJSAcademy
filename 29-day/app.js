7711952e3a97059611c6281dcf20830f7a6a6f70
CadillacJenkins <walter.k.jenkins@gmail.com>
2019-11-16 09:33:14 -0600

    updated solution to reflect chris's solution

--- 29-day/app.js
+++ 29-day/app.js
@@ -1,24 +1,35 @@
-const nodeData = Array.prototype.slice.call(document.querySelectorAll(('h2')));
-const contentList = document.querySelector('#table-of-contents');
+   const init = ()=>{
+        const nodeData = Array.prototype.slice.call(document.querySelectorAll(('h2')));
+        const contentList = document.querySelector('#table-of-contents');
 
-const data = nodeData.map((section, index) =>{
-    if(section.id.length < 1){
-        section.id = section.textContent.split(' ').join('-');
-        return{
-            id: section.id,
-            name: section.textContent
-        }
-    }else{
-        return{
-            id: section.id,
-            name: section.textContent
-            };
-    }
-    
-    });
-console.log(data);
-
-contentList.innerHTML = "<ul>" +
-    data.map((sect, index) =>{
-        return '<input class="content-links f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-black"" type="button" onclick="location.href=`#' + sect.id + '`;" value="' + sect.name + '"/>'
-    }).join('') + "</ul>"
+        contentList.innerHTML = "<ul>" +
+            nodeData.map((section, index) =>{
+                //  WALTER'S ORIGINAL SOLUTION
+                // if(section.id.length < 1){
+                //     section.id = section.textContent.replace(/[^a-z0-9]+/gi, '-');
+                //     return{
+                //         id: section.id,
+                //         name: section.textContent
+                //     }
+                // }else{
+                //     return{
+                //         id: section.id,
+                //         name: section.textContent
+                //         };
+                // }
+                
+                if(section.id.length < 1){
+                    section.id = section.textContent.replace(/[^a-z0-9]+/gi, '-');
+                }
+                console.log(section.textContent);
+                return '<input class="content-links f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-black"" type="button" onclick="location.href=`#' 
+                    + section.id + '`;" value="' + section.textContent + '"/>'
+            }).join('') + "</ul>";
+    };
+        // WALTER'S ORIGINAL SOLUTION
+        // contentList.innerHTML = "<ul>" +
+        //     data.map((sect, index) =>{
+        //         return '<input class="content-links f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-black"" type="button" onclick="location.href=`#' + sect.id + '`;" value="' + sect.name + '"/>'
+        //     }).join('') + "</ul>";
+//    };
+init();
