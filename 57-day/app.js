3791782e6492ff43b40d3b4943cdef4ae1ea019a
AvidDabbler <walter.k.jenkins@gmail.com>
2019-12-09 11:00:31 -0600

    finished day 57

--- 57-day/app.js
+++ 57-day/app.js
@@ -1,21 +1,48 @@
 const app = document.getElementById('app');
-let count = 0;
 
-const timerGo = window.setInterval(() => {
-    count++;
+// POLYFILLS
+if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.msMatchesSelector || 
+                                Element.prototype.webkitMatchesSelector;
+  }
+  
+  if (!Element.prototype.closest) {
+    Element.prototype.closest = function(s) {
+      var el = this;
+  
+      do {
+        if (el.matches(s)) return el;
+        el = el.parentElement || el.parentNode;
+      } while (el !== null && el.nodeType === 1);
+      return null;
+    };
+  }
 
-    if(count < 10){
-        window.clearInterval(timerGo);
-        app.innerHTML = '<h2>Timer Done!!!</h2>'
-        count = 0;
-    }else{
-        app.innerHTML = '<h2>' + count + '</h2>';
-    }
+const timerFunction = () => {
+    let count = {
+        timer: 60
+    };
 
-}, 1000);
+    const timerGo = window.setInterval(() => {
 
-const init = () => {
-    timerGo();
-}
+        console.log('start of timer');
+        count.timer--;
 
-init();
+        if(count.timer < 0){
+            window.clearInterval(timerGo);
+            app.innerHTML = '<h2>Timer Done!!!</h2>'
+        }else{
+            app.innerHTML = '<h2>' + count.timer + '</h2>';
+        }
+
+    }, 10);
+};
+
+const restartTimer = (event) => {
+    if(!event.target.closest('#restart')) return;
+    timerFunction();
+};
+
+timerFunction();
+
+window.addEventListener('click', restartTimer, false)
