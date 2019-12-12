1ab543dfa68077ab323dab43fe895d0bf2e6302b
AvidDabbler <walter.k.jenkins@gmail.com>
2019-12-12 06:36:31 -0600

    clean up

--- 57-day/app.js
+++ 57-day/app.js
@@ -1,22 +1,5 @@
 const app = document.getElementById('app');
 
-// POLYFILLS
-if (!Element.prototype.matches) {
-    Element.prototype.matches = Element.prototype.msMatchesSelector || 
-                                Element.prototype.webkitMatchesSelector;
-  }
-  
-  if (!Element.prototype.closest) {
-    Element.prototype.closest = function(s) {
-      var el = this;
-  
-      do {
-        if (el.matches(s)) return el;
-        el = el.parentElement || el.parentNode;
-      } while (el !== null && el.nodeType === 1);
-      return null;
-    };
-  }
 
 const duration = 120
 
@@ -29,7 +12,6 @@ const template = () => {
     if(count.done) return '<h2>Timer\'s Done</h2><button id="restart">Restart Timer</button>';
     let min = Math.floor(count.timer / 60).toString().padStart(1,0);
     let sec = (count.timer - (min * 60)).toString().padStart(2,0);
-    console.log(sec)
     return min + ':' + sec;
 };
 
@@ -54,7 +36,7 @@ const run = ()=>{
         if(done){
             window.clearInterval(start);
         }
-    }, 100);
+    }, 1000);
 }
 
 run();
