f0805baaa9b4a72fb6bc7db7bd08d35286fc344f
AvidDabbler <walter.k.jenkins@gmail.com>
2019-12-13 13:06:52 -0600

    fixed pause isse with rendering new buttons

--- 57-day/app.js
+++ 57-day/app.js
@@ -1,18 +1,24 @@
 const app = document.getElementById('app');
 
-
 const duration = 120
 
 let count = {
-    timer: 60,
-    done: false
+    timer: duration,
+    done: false,
+    pause: false,
+    button: 'pause'
 };
 
+let button = () => document.getElementById('button').innerHTML = '<button class="button" id="' + count.button +'">' + count.button +'</button>'
+
 const template = () => {
-    if(count.done) return '<h2>Timer\'s Done</h2><button id="restart">Restart Timer</button>';
+    if(count.done) return '<h2>Timer\'s Done</h2>';
     let min = Math.floor(count.timer / 60).toString().padStart(1,0);
-    let sec = (count.timer - (min * 60)).toString().padStart(2,0);
-    return min + ':' + sec;
+    let sec = (count.timer % 60).toString().padStart(2,0);
+    let clock = '<h2>' + min + ':' + sec + '</h2>';
+
+    if(count.pause) return clock;
+    return clock;
 };
 
 var render = function () {
@@ -21,8 +27,9 @@ var render = function () {
 };
  
 const run = ()=>{ 
-    count.timer = duration;
+    count.button = 'pause';
     count.done = false;
+    count.pause = false;
 
     const start =window.setInterval(() => {
         let time = count.timer - 1;
@@ -33,17 +40,44 @@ const run = ()=>{
 
         render();
 
-        if(done){
+        if(done || count.pause){
             window.clearInterval(start);
         }
+        if(done){
+            count.button = 'restart';
+            render();
+            button();
+        }
     }, 1000);
+};
+
+const restart = () => {
+    count.timer = duration
+    run();
+};
+
+const pause = () => {
+    count.pause = true;
+    count.button = 'start';
 }
 
 run();
+button();
 
+const close = (id) => {
+    document.getElementById(id).closest('.button');
+}
 
 window.addEventListener('click', ()=>{
-    if(!document.getElementById('restart')) return;
-    run();
-
+    if(!document.querySelector('.button')) return;
+    else if(event.target.closest('#restart')){
+         restart();
+    }
+    else if(event.target.closest('#pause')){
+        pause();
+    }
+    else if (event.target.closest('#start')) {
+        run();
+    }
+    button();
 }, false)
