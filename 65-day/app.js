fa406064953e8275fd86facfe8f99f6fc2f38b49
AvidDabbler <walter.k.jenkins@gmail.com>
2019-12-29 16:30:48 -0600

    attempt to add in checked styling

--- 65-day/app.js
+++ 65-day/app.js
@@ -13,7 +13,7 @@ var app = new Reef('#app', {
         return '<ul class="todos">' + props.todos.map((el, i) => {
             let item = 
                 '<label for="todo-' + i + '">' + 
-                        '<input id="todo-' + i + '" type="checkbox">' + el +
+                        '<input id="todo-' + i + '" type="checkbox" class="checkbox unchecked">' + el +
                         '<button class="delete" data-id ="' + i + '">Delete</button>'
                     '</label>';
             return item;
@@ -26,6 +26,7 @@ const val = todo.value;
 const update = () => {
     app.data.todos = JSON.parse(localStorage.getItem('data'));  
     console.log('update');
+    
     app.render();
 }
 
@@ -56,14 +57,27 @@ const addData = () => {
     update();
 }
 
-const deleteItem = (event) => {
-    if(!event.target.closest('.delete'))return;
-    let delete_id = event.target.dataset.id;
-
-    app.data.todos.splice(delete_id, 1);
-    localStorage.setItem('data', JSON.stringify(list()));
-    app.render();
-    console.log('delete')
+const click = (event) => {
+    if(event.target.closest('.delete')){
+        let delete_id = event.target.dataset.id;
+        app.data.todos.splice(delete_id, 1);
+        localStorage.setItem('data', JSON.stringify(app.data.todos));
+        app.render();
+        console.log('delete')
+        return;
+    }
+    // else if(event.target.closest('.checkbox')){
+    //     if(event.target.closest('.unchecked')){
+    //         event.target.classList.remove('unchecked');
+    //         event.target.classList.add('checked');
+    //         return;
+    //     }else if(event.target.closest('.checked')){
+    //         event.target.classList.remove('checked');
+    //         event.target.classList.add('unchecked');
+    //         return
+    //     }
+    // }
+    
 
 }
 
@@ -74,5 +88,5 @@ const load = () => {
 update();
 
 window.addEventListener("submit", addData, false);
-window.addEventListener("click", deleteItem, false);
+window.addEventListener("click", click, false);
 window.addEventListener('load', load, false);
